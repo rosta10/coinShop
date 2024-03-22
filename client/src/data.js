82e9a8f8b3d94020374dc64876f92a3f275e1cb6
rosta10 <58655450+rosta10@users.noreply.github.com>
2024-03-22 16:56:13 +0100

    Create data.js

---
+++ client/src/data.js
@@ -0,0 +1,44 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://img41.rajce.idnes.cz/d4102/18/18493/18493889_c9bdb110fac72ad76ff759007b1247dc/images/343391306_799212328095385_2637045958092324025_n.jpg?ver=0",
+    title: "Papírové bankovky",
+    desc: "České i zahraniční papírové bankovky.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://img41.rajce.idnes.cz/d4102/18/18493/18493889_c9bdb110fac72ad76ff759007b1247dc/images/343740908_812242959913637_3617171051029650802_n.jpg?ver=0",
+    title: "Mince",
+    desc: "Zlaté a stříbrné mince, jak investiční, tak sběratelské",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://img41.rajce.idnes.cz/d4102/18/18493/18493889_c9bdb110fac72ad76ff759007b1247dc/images/345234059_286979803685400_5974547395606446888_n.jpg?ver=0",
+    title: "Známky",
+    desc: "Sběralské známky všeho druhu",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://img41.rajce.idnes.cz/d4102/18/18493/18493889_c9bdb110fac72ad76ff759007b1247dc/images/banknote.jpg?ver=0",
+    title: "Papírové bankovky",
+    cat: "banknote",
+  },
+  {
+    id: 2,
+    img: "https://img41.rajce.idnes.cz/d4102/18/18493/18493889_c9bdb110fac72ad76ff759007b1247dc/images/coins.jpg?ver=0",
+    title: "Mince",
+    cat: "coin",
+  },
+  {
+    id: 3,
+    img: "https://img41.rajce.idnes.cz/d4102/18/18493/18493889_c9bdb110fac72ad76ff759007b1247dc/images/stamps.jpg?ver=0",
+    title: "Poštovní známky",
+    cat: "stamps",
+  },
+];
