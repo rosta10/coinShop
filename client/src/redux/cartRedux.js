51099838e5ca323c722de9fd22ccc2f7b58ad8fa
rosta10 <58655450+rosta10@users.noreply.github.com>
2024-03-23 16:38:51 +0100

    Update cartRedux.js

--- client/src/redux/cartRedux.js
+++ client/src/redux/cartRedux.js
@@ -6,12 +6,15 @@ const cartSlice = createSlice({
     products: [],
     quantity: 0,
     total: 0,
+    shippingCost: 150,
+    shippingDiscount: 0,
   },
   reducers: {
     addProduct: (state, action) => {
           state.quantity += 1;
           state.products.push(action.payload);
-          state.total += action.payload.price * action.payload.quantity; 
+          state.total += action.payload.price * action.payload.quantity;
+          state.shippingDiscount = state.total >= 1000 ? -state.shippingCost : 0;
       },
 
     removeFromCart(state, action) {
@@ -28,8 +31,7 @@ const cartSlice = createSlice({
       });
       state.quantity -= 1;
       state.total -= action.payload.price * action.payload.quantity;
-      //state.quantity = 0;
-      //state.total = 0;
+      state.shippingDiscount = state.total >= 1000 ? -state.shippingCost : 0;
     },
 
     decreaseQuantity(state, action) {
@@ -38,15 +40,12 @@ const cartSlice = createSlice({
           if(cartProduct.quantity > 1){
             cartProduct.quantity -= 1;
             state.total -= action.payload.price;
+            state.shippingDiscount = state.total >= 1000 ? -state.shippingCost : 0;
           }
         }
         localStorage.setItem("products", JSON.stringify(state.products));
         return state;
       });
-      
-
-      //state.quantity = 0;
-      //state.total = 0;
     },
 
     increaseQuantity(state, action) {
@@ -54,23 +53,13 @@ const cartSlice = createSlice({
         if (cartProduct._id === action.payload._id) {
           cartProduct.quantity += 1;
           state.total += action.payload.price;
+          state.shippingDiscount = state.total >= 1000 ? -state.shippingCost : 0;
         }
         
         localStorage.setItem("products", JSON.stringify(state.products));
         return state;
       });
-      
-
-      //state.quantity = 0;
-      //state.total = 0;
     },
-
-    handleShipping(state, action) {
-      if (state.total >= 1000){
-        
-      }
-    },
-
   },
 });
 
